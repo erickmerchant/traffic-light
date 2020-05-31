4b9d98f778dc4674ea117f15307fd8237789a636
Erick Merchant <erickmerchant@gmail.com>
2020-05-31 17:44:13 -0400

    Replace with the actual circle

--- src/app.mjs
+++ src/app.mjs
@@ -23,18 +23,16 @@ export const createComponent = (classes) => (state) => html`
     <svg class=${classes.lights} viewBox=${`0 0 10 ${colors.length * 10}`}>
       ${colors.map(
         (color, index) => html`
-          <use
-            y=${index * 10}
-            href="#light"
+          <circle
+            r="4"
+            cx="5"
+            cy=${index * 10 + 5}
             class=${classes[
               sequence[state % sequence.length] === color ? color : 'gray'
             ]}
           />
         `
       )}
-      <defs>
-        <circle id="light" cx="5" cy="5" r="4" />
-      </defs>
     </svg>
   </body>
 `
