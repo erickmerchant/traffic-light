ee76800a2c137b8ca95226b496daa1624a3e6bdd
Erick Merchant <erickmerchant@gmail.com>
2020-11-30 18:46:16 -0500

    Improve spawn

--- cli.js
+++ cli.js
@@ -4,9 +4,19 @@ import childProcess from 'child_process'
 const options = {stdio: 'inherit'}
 const command = process.argv[2]
 
-const spawn = (...args) =>
+const spawn = (strs, ...quoted) =>
   new Promise((resolve) => {
-    const spawned = childProcess.spawn(...args)
+    const args = []
+
+    for (let i = 0; i < strs.length; i++) {
+      args.push(...strs[i].split(' '))
+
+      if (quoted[i] != null) {
+        args.push(quoted[i])
+      }
+    }
+
+    const spawned = childProcess.spawn(args[0], args.slice(1), options)
 
     spawned.on('exit', () => {
       resolve()
@@ -16,9 +26,9 @@ const spawn = (...args) =>
 const program = async () => {
   try {
     if (command === 'start') {
-      spawn('css', ['src/styles.js', 'dist/css', '-wd'], options)
+      spawn`css src/styles.js dist/css -wd`
 
-      spawn('dev', ['serve', 'src', 'dist', '-de', 'dev.html'], options)
+      spawn`dev serve src dist -de dev.html`
     }
   } catch (error) {
     console.error(error)
