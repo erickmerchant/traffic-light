13386eb5b8c6956228e2b0f4e51a1a1178baae73
Erick Merchant <erickmerchant@gmail.com>
2021-02-05 00:32:53 -0500

    Introduce sergeant

--- cli.js
+++ cli.js
@@ -1,27 +1,7 @@
 #!/usr/bin/env node
-import childProcess from 'child_process'
+import {spawn} from 'sergeant'
 
-const options = {stdio: 'inherit'}
 const command = process.argv[2]
-
-const spawn = (strs, ...quoted) =>
-  new Promise((resolve) => {
-    const args = []
-
-    for (let i = 0; i < strs.length; i++) {
-      args.push(...strs[i].split(' '))
-
-      if (quoted[i] != null) {
-        args.push(quoted[i])
-      }
-    }
-
-    const spawned = childProcess.spawn(args[0], args.slice(1), options)
-
-    spawned.on('exit', () => {
-      resolve()
-    })
-  })
 
 const program = async () => {
   try {
